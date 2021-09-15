d0de58413841c1f4dbae6ca22aefeac88bf7f660
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-15 19:49:29 +0100

    find build zip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-echo"##############"
-echo"#  PATCHKIT  #"
-echo"##############"
+echo "##############"
+echo "#  PATCHKIT  #"
+echo "##############"
 
 curl -L "https://github.com/patchkit-net/patchkit-tools/releases/download/v3.1.2/patchkit-tools-linux-x86_64.zip" --output patchkit-tools-linux-x86_64.zip
 unzip -o patchkit-tools-linux-x86_64.zip -d patchkit/
 
-echo"##############"
-echo"#    BUILD   #"
-echo"##############"
-echo $path
+echo "##############"
+echo "#    BUILD   #"
+echo "##############"
+find / -name "build.zip"
+
 curl -L $path --output output.zip
 unzip -o patchkit-tools-linux-x86_64.zip -d patchkit/output
 
-echo"##############"
-echo"#   UPLOAD   #"
-echo"##############"
+echo "##############"
+echo "#   UPLOAD   #"
+echo "##############"
 chmod -R +x patchkit/
 cd ./patchkit
 bash ./patchkit-tools make-version -s $secret-key -a $api-key -l github_actions -f ./output -x
