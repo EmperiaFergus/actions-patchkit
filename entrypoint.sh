b90f34bd5ccde10555d08853c06b980ffb72c266
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-15 17:55:09 +0100

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-echo "$(application/vnd.github.v3+json)"
+
+echo"##############"
+echo"#  PATCHKIT  #"
+echo"##############"
+
 curl -L "https://github.com/patchkit-net/patchkit-tools/releases/download/v3.1.2/patchkit-tools-linux-x86_64.zip" --output patchkit-tools-linux-x86_64.zip
 unzip -o patchkit-tools-linux-x86_64.zip -d patchkit/
 
+echo"##############"
+echo"#    BUILD   #"
+echo"##############"
 echo $path
 curl -L $path --output output.zip
 unzip -o patchkit-tools-linux-x86_64.zip -d patchkit/output
 
+echo"##############"
+echo"#   UPLOAD   #"
+echo"##############"
 chmod -R +x patchkit/
 cd ./patchkit
 bash ./patchkit-tools make-version -s $secret-key -a $api-key -l github_actions -f ./output -x
