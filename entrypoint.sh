3eeef297417e0c10efe24e0fc5abe0d7865cb219
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-15 17:33:46 +0100

    amend default message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+echo "$(application/vnd.github.v3+json)"
 curl -L "https://github.com/patchkit-net/patchkit-tools/releases/download/v3.1.2/patchkit-tools-linux-x86_64.zip" --output patchkit-tools-linux-x86_64.zip
 unzip -o patchkit-tools-linux-x86_64.zip -d patchkit/
 
@@ -8,4 +8,4 @@ unzip -o patchkit-tools-linux-x86_64.zip -d patchkit/output
 
 chmod -R +x patchkit/
 cd ./patchkit
-bash ./patchkit-tools make-version -s $secret-key -a $api-key -l "uploaded using github actions" -f ./output -x
+bash ./patchkit-tools make-version -s $secret-key -a $api-key -l github_actions -f ./output -x
