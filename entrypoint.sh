ba7c2816117bd6416048fa3da2ec76d5dbf45ac0
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-16 23:34:02 +0100

    clean entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,35 +17,21 @@ echo $API_KEY
 echo $ZIP_PATH
 
 ls -R $ZIP_PATH
-#curl -L $ZIP_PATH --output output.zip
-echo "current directory"
-pwd
-#unzip -o "$ZIP_PATH"/build.zip -d ./patchkit/output/
-#we can only upload a folder with one exe
+
+
 rm "patchkit/output/UnityCrashHandler32.exe"
 rm "patchkit/output/UnityCrashHandler64.exe"
 
 echo "##############"
 echo "#   UPLOAD   #"
 echo "##############"
-ls -R
-chmod -R +x patchkit/
-echo "moving to patchkit"
-cd ./patchkit
 
-#if [ -s app/core/utils/progress_bar.rb]; then
+chmod -R +x patchkit/
+cd ./patchkit
 if [ -s app/core/utils/progress_bar.rb ]; then
   echo "progress bar exists"
 fi
 
 sed -i 's/IO\.console\.winsize\[1\]/800/g' app/core/utils/progress_bar.rb
-
 sed -n '21p' < app/core/utils/progress_bar.rb
-#if [ -s changelog.txt ]; then
-#  echo "changes made!"
-#fi  
-#ls
-#echo "current directory"
-#pwd
 bash ./patchkit-tools make-version -s "$SECRET_KEY" -a "$API_KEY" -l github_actions -f $ZIP_PATH -x
-#bash ./patchkit-tools make-version -s "ac1ae6ae296777d8f700b72ea5231cc8" -a "ccfb4cd4e4aea80d14fcc2b649001f0b" -l github_actions -f ./ -x
