6b1856ece86f00c42c9b459e9aa6d6bc9e9664b4
EmperiaFergus <Fergus@emperia.co.uk>
2021-09-16 18:11:07 +0100

    ls

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,5 +41,6 @@ sed -n '21p' < app/core/utils/progress_bar.rb
 #if [ -s changelog.txt ]; then
 #  echo "changes made!"
 #fi  
-bash ./patchkit-tools make-version -s "$SECRET_KEY" -a "$API_KEY" -l github_actions -f ./output/ -x
+ls
+bash ./patchkit-tools make-version -s "$SECRET_KEY" -a "$API_KEY" -l github_actions -f ./output -x
 #bash ./patchkit-tools make-version -s "ac1ae6ae296777d8f700b72ea5231cc8" -a "ccfb4cd4e4aea80d14fcc2b649001f0b" -l github_actions -f ./ -x
